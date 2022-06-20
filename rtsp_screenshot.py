e62ed42b7a7d14955f8005cfe87d58e06a96cc4c
Colin Kilgour <kilgourcolin@gmail.com>
2022-06-19 20:43:16 -0600

    Build function

--- rtsp_screenshot.py
+++ rtsp_screenshot.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python3
-#Grab an image from an RTSP Stream. Used to grab static image from escalator cameras to assist with sign setup. 
+#Grab an image from an RTSP Stream. Returns cv2 image
 import cv2 #open-cv lib is needed for RTSP stream.
 import os
 import sys
+from copy import deepcopy
 
 def rtsp_screenshot():
-    ipcams = ['192.168.4.190','192.168.4.204'] #list of hosts to grab data from.
+    ipcam = '' #list of hosts to grab data from.
     username = ''
     password = ''
     port = '554' #udp port number
-    imagefiles = [] #array of files we've created. 
-    combinedimagefile = 'combined3478dm38fgm4mnrs.jpg'
 
-    for ipcam in ipcams:
-        try:
+    try:
+        if username != '' or password != '': 
             RTSP_URL = 'rtsp://' + username + ':' + password + '@' + ipcam + ':' + port
+        else: #No auth
+            RTSP_URL = 'rtsp://' + ipcam + ':' + port
 
-            os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
-            
-            cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
-            
-            if not cap.isOpened():
-                print('Cannot open RTSP stream ' + ipcam)
-                #exit(-1)
-                pass
-
-            filename = 'cam' + ipcam + '.jpg'
-            imagefiles.append(filename)
+        os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
+        
+        cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
+        
+        if not cap.isOpened():
+            return('Cannot open RTSP stream ' + ipcam)
+            exit(-1)
+            pass
+        else:
             ret, frame = cap.read()
-            cv2.imwrite(filename,frame)
+            returnimage = deepcopy(frame) #copy pic before clean up
 
+            #Clean up our CV2 data
             cap.release()
             cv2.destroyAllWindows()
-        except:
-            print("Unable to get data from camera. " + ipcam)
 
-    #Combine to one image. 
-    cv2images = []
-    for imagefile in imagefiles:
-        cv2images.append(cv2.imread(imagefile))
-    im_v = cv2.vconcat(cv2images)
-    cv2.imwrite(combinedimagefile, im_v)
+    except:
+        return("Unable to get data from camera. " + ipcam)
 
-
+    return(returnimage)
